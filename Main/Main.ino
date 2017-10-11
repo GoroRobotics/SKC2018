bff0c3d9440fdad3580755ae4b8af5d58f9b1ff4
CDFER <Christopher@dirksonline.net>
2017-10-11 22:10:10 +1300

    Added Goro Themed text (and scroll away text)

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,45 +1,47 @@
-/*
-This example shows how to fade the built in LED using the analogWrite() function.
-
-The analogWrite() function uses PWM, so if you want to change the pin you're 
-using, be sure to use another PWM capable pin. (denoted by ~) 
-*/
-
 /*-----( Import needed libraries )-----*/
 #include <Wire.h>
 #include <NewliquidCrystal\LiquidCrystal_I2C.h>
 
+/*-----(       Definitions       )-----*/
 #define LED LED_BUILTIN		// LED is the built in LED pin
+
+/*-----(       Variables         )-----*/
 int brightness = 0;			// how bright the LED is
 int fadeAmount = 5;			// how many points to fade the LED by
 int fadespeed  = 15;		//the speed of the fade (in ms)
-LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  //TODO: Find and Set the LCD I2C address
+
+/*-----(    Set I2C adresses     )-----*/
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  //Set the LCD I2C address
 
 
 void setup() {
-	pinMode(LED, OUTPUT);	// declare LED to be an output:
-	lcd.begin(16, 2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
-						// ------- Quick 3 blinks of backlight  -------------
-	for (int i = 0; i< 3; i++)
-	{
-		lcd.backlight();
-		delay(250);
-		lcd.noBacklight();
-		delay(250);
-	}
-	lcd.backlight(); // finish with backlight on  
+	pinMode(LED, OUTPUT);		// declare LED to be an output:
+	lcd.begin(16, 2);			// initialize the lcd for 16 chars 2 lines
+	lcd.noBacklight();			//Blink backlight
+	lcd.backlight();		
+	
 
-	//-------- Write characters on the display ------------------
-	// NOTE: Cursor Position: (CHAR, LINE) start at 0  
-	lcd.setCursor(0, 0); //Start at character 4 on line 0
+	//--- Write on the display (Title)---
+	lcd.setCursor(2, 0);		// NOTE: Cursor Position: (CHAR, LINE) start at 0
 	lcd.print("GORO PHEONIX");
 	delay(1000);
-	lcd.setCursor(0, 1);
-	lcd.print("Hello, world!");
-	delay(8000);
-	lcd.setCursor(0, 1);
-	lcd.print("Risng frm ashes");
-	delay(8000);
+	lcd.setCursor(2, 1);
+	lcd.print("Hello, world");
+	delay(1000);
+
+	//---Scroll away text---
+	for (int i = 0; i< 16; i++){
+		lcd.scrollDisplayLeft();
+		delay(150);
+	}//end for loop
+	lcd.clear();
+
+	//--- Write on the display (Subtitle)---
+	lcd.setCursor(2, 0);
+	lcd.print("Rising from");
+	delay(500);
+	lcd.setCursor(3, 1);
+	lcd.print("the ashes");
 
 }//end setup
 
