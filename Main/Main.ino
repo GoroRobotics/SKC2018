8e0dcf4ce8ce7589a48521f0af4f825be7e2b26b
CDFER <Christopher@dirksonline.net>
2017-10-11 16:23:15 +1300

    Fade sketch implemented
    Added and then edited fade example sketch

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,13 +1,30 @@
-void setup()
-{
+/*
+This example shows how to fade the built in LED using the analogWrite() function.
 
-  /* add setup code here */
+The analogWrite() function uses PWM, so if you want to change the pin you're 
+using, be sure to use another PWM capable pin. (denoted by ~) 
+*/
 
-}
+#define LED LED_BUILTIN		// LED is the built in LED pin
+int brightness = 0;			// how bright the LED is
+int fadeAmount = 5;			// how many points to fade the LED by
+int speed = 30;				//the speed of the fade (in ms)
+	
 
-void loop()
-{
+void setup() {
+	pinMode(LED, OUTPUT);	// declare LED to be an output:
+}//end setup
 
-  /* add main program code here */
 
-}
+// the loop routine runs over and over again forever:
+void loop() {
+	
+	analogWrite(LED, brightness);				// set the brightness of the led:
+
+	brightness = brightness + fadeAmount;		// change the brightness for next time through the loop:
+	
+	if (brightness <= 0 || brightness >= 255){	// reverse the direction of the fading at the ends of the fade:
+		fadeAmount = -fadeAmount;
+	}//end if
+	
+}//end loop
