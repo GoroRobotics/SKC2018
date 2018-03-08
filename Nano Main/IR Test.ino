c8f9529d95fc1a0117eb5186505f75de8b5cff3b
CD_FER <Christopher@dirksonline.net>
2018-03-08 19:55:53 +1300

    add Nano main, add IR test.ino and tested

--- Nano Main/IR Test.ino
+++ Nano Main/IR Test.ino
@@ -1,14 +1,24 @@
 
 //Port C
-#define LED_1 A2
+#define LED_1 A0
 #define LED_2 A1
-#define LED_3 A0
+#define LED_3 A2
+#define LED_4 A3
+#define LED_5 A4
+#define LED_6 A5
+
 
 //Port D
-#define BTN_1 8
-#define BTN_2 9
-#define BTN_3 10
-#define BTN_MASK B00000111
+#define IR_1 1
+#define IR_2 2
+#define IR_3 3
+#define IR_4 4
+#define IR_5 5
+#define IR_6 6
+#define IR_7 7
+#define IR_8 8
+
+#define IR_MASK B00000111
 
 #define DELAY_MS 100
 
@@ -19,12 +29,20 @@ void setup() {
 	pinMode(LED_1, OUTPUT);
 	pinMode(LED_2, OUTPUT);
 	pinMode(LED_3, OUTPUT);
-	pinMode(BTN_1, INPUT_PULLUP);
-	pinMode(BTN_2, INPUT_PULLUP);
-	pinMode(BTN_3, INPUT_PULLUP);
+	pinMode(LED_4, OUTPUT);
+	pinMode(LED_5, OUTPUT);
+	pinMode(LED_6, OUTPUT);
+	pinMode(IR_1, INPUT_PULLUP);
+	pinMode(IR_2, INPUT_PULLUP);
+	pinMode(IR_3, INPUT_PULLUP);
+	pinMode(IR_4, INPUT_PULLUP);
+	pinMode(IR_5, INPUT_PULLUP);
+	pinMode(IR_6, INPUT_PULLUP);
+	pinMode(IR_7, INPUT_PULLUP);
+	pinMode(IR_8, INPUT_PULLUP);
 
-	Serial.begin(57600);
-	Serial.println("*** Begin ***");
+	//Serial.begin(57600);
+	//Serial.println("*** Begin ***");
 }
 
 byte inputs_now = 0, inputs_prev = 0;
@@ -34,11 +52,11 @@ void loop() {
 	
 	do {
 		//read inputs on POPRTB
-		inputs_now = (~PINB & BTN_MASK);
+		inputs_now = (~PINB & IR_MASK);
 	} while (inputs_now == inputs_prev); //loop untill input changes
 	inputs_prev = inputs_now;
 
-	Serial.println(inputs_now);// | B00000000, BIN);
+	//Serial.println(inputs_now);// | B00000000, BIN);
 
 	//set LEDs
 	PORTC = inputs_now;
