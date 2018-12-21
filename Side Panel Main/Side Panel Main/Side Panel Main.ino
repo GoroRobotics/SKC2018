0bb07035660f080dcb354b7b6fe740e28736a887
CD_FER <Christopher@dirksonline.net>
2018-12-21 19:32:41 +1300

    Tested side panel bink and organised files

---
+++ Side Panel Main/Side Panel Main/Side Panel Main.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Side_Panel_Main.ino
+ Created:	12/21/2018 4:39:26 PM
+ Author:	CD_FER
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
